30dfee18dfae6da66ff3779109eb787ce09f1393
thread <thethread@gmail.com>
2016-01-19 00:00:11 -0500

    syntax

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -8,20 +8,18 @@ defmodule Tuesday.PageController do
   def info(conn, _params) do
     url = "http://techtues.net/stat"
 
-    case HTTPoison.get(url) do
+    count = case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        count = body |> extract_count
+        body |> extract_count
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        count = "unknown"
+        "unknown"
       {:error, %HTTPoison.Error{reason: reason}} ->
-        count = "unknown"
+        "unknown"
     end
 
-    info = %{
+    json conn, %{
       count: count
     }
-
-    json conn, info
   end
 
   defp extract_count(body) do
