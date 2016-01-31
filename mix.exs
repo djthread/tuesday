a61e5f7f74e726eb7993d7514ef28323c41bba39
thread <thethread@gmail.com>
2016-01-31 02:39:04 -0500

    trying ansible...

--- mix.exs
+++ mix.exs
@@ -2,8 +2,13 @@ defmodule Tuesday.Mixfile do
   use Mix.Project
 
   def project do
+    {result, _exit_code} = System.cmd("git", ["rev-parse", "HEAD"])
+
+    # We'll truncate the commit SHA to 7 chars. Feel free to change
+    git_sha = String.slice(result, 0, 7)
+
     [app: :tuesday,
-     version: "0.0.1",
+     version: "0.0.1-#{git_sha}",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
