ac513d34a491fc833682b7e39199987f6173dd4a
thread <thethread@gmail.com>
2016-05-20 18:19:23 -0400

    .

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :tuesday, Tuesday.Endpoint,
-  http: [port: 4000],
+  http: [port: 4091],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
@@ -34,14 +34,16 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
-config :tuesday, Tuesday.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "tuesday_dev",
-  hostname: "localhost",
-  pool_size: 10
+# config :tuesday, Tuesday.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   username: "postgres",
+#   password: "postgres",
+#   database: "tuesday_test",
+#   hostname: "localhost",
+#   pool_size: 10
 
 config :tuesday, :podcast_paths, %{
   "techno-tuesday" => "/Users/thread/Desktop/impulse/thread-episodes"
 }
+
+import_config "dev.secret.exs"
