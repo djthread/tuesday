f35e48127de9fd1b78be44a4b9310a6ac5e1d815
thread <thethread@gmail.com>
2017-02-13 15:57:11 -0500

    ..

--- web/elm/src/Public/Page/Home/View.elm
+++ web/elm/src/Public/Page/Home/View.elm
@@ -1,6 +1,6 @@
 module Page.Home.View exposing (..)
 
-import Html exposing (Html, div, a, text, footer)
+import Html exposing (Html, div, a, p, text, footer)
 import Html.Attributes exposing (class, href)
 import Html.Events exposing (onClick)
 import Types exposing (..)
@@ -8,11 +8,12 @@ import Types exposing (..)
 root : Model -> Html Msg
 root model =
   div [class "row"]
-    [ div [class "column small-6"]
+    [ div [class "large-6 columns"]
       [ text "hello"
       , a [href "#about"] [text "About page"]
+      , p [] [text "And so it was, some things and stuff about whatever to make a longer line of text."]
       ]
-    , div [class "column small-6"]
+    , div [class "large-6 columns"]
         [ div [] [text "yayy"]
         , div [] [text "yayy"]
         , div [] [text "yayy"]
