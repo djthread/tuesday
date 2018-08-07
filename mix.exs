2e08cf16ad6c51704203e09874d6f8c59547174c
thread <thethread@gmail.com>
2018-08-07 17:35:30 -0400

    updated some packages

--- mix.exs
+++ mix.exs
@@ -59,7 +59,9 @@ defmodule Tuesday.Mixfile do
       # {:exrm,                "~> 1.0.0"},
       {:distillery,          "~> 1.0"},
       {:cors_plug,           "~> 1.1"},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+
+      {:ex_config, git: "https://github.com/djthread/ex_config.git"}
 
       # {:wallaby,             "~> 0.16.1"}
 # <<<<<<< dfb6bd034e25e6b5f5f354d2e75c99d1461c1271
